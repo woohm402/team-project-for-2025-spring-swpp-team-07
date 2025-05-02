e29d9e5b18b500be0dabe103f9380115bc0d5101
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2025-05-02 14:55:33 +0900

    ✨ daily scrum 에서 오늘 할 일 보여주기 (#19)
    ### before
    <img width="542" alt="image"
    src="https://github.com/user-attachments/assets/5e880806-5814-48db-a9ab-1f8cfeb63869"
    />
    
    ### after
    <img width="542" alt="image"
    src="https://github.com/user-attachments/assets/feab67a4-ad55-4d82-94f7-8981b56c9647"
    />

--- scripts/daily-scrum/send.ts
+++ scripts/daily-scrum/send.ts
@@ -1,23 +1,95 @@
 import { Octokit } from "@octokit/rest";
 import { WebClient } from "@slack/web-api";
+import { Client } from "@notionhq/client";
 
 const SLACK_CHANNEL = import.meta.env.SLACK_CHANNEL;
 const SLACK_BOT_TOKEN = import.meta.env.SLACK_BOT_TOKEN;
+const NOTION_TOKEN = import.meta.env.NOTION_TOKEN;
 
-if (SLACK_CHANNEL === undefined || SLACK_BOT_TOKEN === undefined) {
-  throw new Error("SLACK_CHANNEL or SLACK_BOT_TOKEN is not defined");
+if (
+  SLACK_CHANNEL === undefined ||
+  SLACK_BOT_TOKEN === undefined ||
+  NOTION_TOKEN === undefined
+) {
+  throw new Error(
+    "SLACK_CHANNEL or SLACK_BOT_TOKEN or NOTION_TOKEN is not defined",
+  );
 }
 
+const notionClient = new Client({ auth: NOTION_TOKEN });
 const octokit = new Octokit();
 const slackClient = new WebClient(SLACK_BOT_TOKEN);
 
 const members = [
-  { github: "woohm402", slack: "U08HG6Q15NK" },
-  { github: "cloNoey", slack: "U08HG6P8JJ3" },
-  { github: "SummerPea", slack: "U08HZ8HKUDT" },
-  { github: "kd00172", slack: "U08HG6NS7B9" },
-  { github: "mn39", slack: "U08HZ8KAS5T" },
+  {
+    github: "woohm402",
+    slack: "U08HG6Q15NK",
+    notion: "a60a2b22-e58c-4cf8-a100-764f60cac65c",
+  },
+  {
+    github: "cloNoey",
+    slack: "U08HG6P8JJ3",
+    notion: "d747c822-e583-4eee-9f42-edf7baea585b",
+  },
+  {
+    github: "SummerPea",
+    slack: "U08HZ8HKUDT",
+    notion: "112d872b-594c-8160-a74b-00026265457c",
+  },
+  {
+    github: "kd00172",
+    slack: "U08HG6NS7B9",
+    notion: "9cd9e79b-3274-41a5-aa5a-e6b1ee0ff019",
+  },
+  {
+    github: "mn39",
+    slack: "U08HZ8KAS5T",
+    notion: "96689d8e-1cf1-4bf5-9ec9-caab076bd9f6",
+  },
 ];
+
+const tasks = await notionClient.databases.query({
+  database_id: "1e19614fd0a380089653e1dd33ff6506",
+  filter: {
+    and: [
+      { property: "스프린트", select: { equals: "Sprint 1 (4/28-5/11)" } },
+      { property: "상태", status: { does_not_equal: "완료" } },
+    ],
+  },
+});
+
+const targetTasks = tasks.results
+  .flatMap((r) => {
+    if (r.object !== "page" || !("properties" in r)) throw new Error();
+    const titleProperty = r.properties["이름"];
+    const assigneeProperty = r.properties["담당자"];
+    const expectedScheduleProperty = r.properties["예상 일정"];
+
+    if (
+      titleProperty.type !== "title" ||
+      assigneeProperty.type !== "people" ||
+      expectedScheduleProperty.type !== "date" ||
+      expectedScheduleProperty.date === null ||
+      expectedScheduleProperty.date.end === null
+    )
+      throw new Error();
+
+    const assignee = members.find(
+      (m) => m.notion === assigneeProperty.people[0].id,
+    );
+
+    if (!assignee) throw new Error();
+
+    return {
+      title: titleProperty.title.map((t) => t.plain_text).join(" "),
+      assignee,
+      schedule: {
+        start: expectedScheduleProperty.date.start,
+        end: expectedScheduleProperty.date.end,
+      },
+    };
+  })
+  .filter((r) => Date.now() >= new Date(r.schedule.start).getTime());
 
 const githubUsernameToSlackMention = (githubUsername: string) => {
   const found = members.find((member) => member.github === githubUsername);
@@ -93,19 +165,34 @@ await slackClient.chat.postMessage({
       type: "section",
       text: {
         type: "mrkdwn",
-        text: members
-          .map((m) => githubUsernameToSlackMention(m.github))
-          .join(" "),
+        text: "어제 한 일과 오늘 할 일을 스레드 댓글로 공유해 주세요! 병목이나 문제가 있다면 같이 알려주세요.",
       },
+    },
+    { type: "divider" },
+    {
+      type: "section",
+      text: { type: "plain_text", text: "할 일" },
     },
     {
       type: "section",
       text: {
         type: "mrkdwn",
-        text: "어제 한 일과 오늘 할 일을 스레드 댓글로 공유해 주세요! 병목이나 문제가 있다면 같이 알려주세요.",
+        text: targetTasks
+          .sort(
+            (a, b) =>
+              new Date(a.schedule.end).getTime() -
+              new Date(b.schedule.end).getTime(),
+          )
+          .map((task) =>
+            [
+              `*\`~${task.schedule.end.slice("2025-".length).replace("-", ".")}\`*`,
+              githubUsernameToSlackMention(task.assignee.github),
+              task.title,
+            ].join(" "),
+          )
+          .join("\n"),
       },
     },
-    { type: "divider" },
   ],
   text: `어제 한 일과 오늘 할 일을 스레드 댓글로 공유해 주세요! 병목이나 문제가 있다면 같이 알려주세요.`,
 });
