7263bb10e673e67be6fbdf17e5e5cfe13e0c96c8
mn39 <48751102+mn39@users.noreply.github.com>
2025-05-25 23:28:57 +0900

    checkpoint (#64)
    ## Description
    
    체크포인트를 만들었고 체크포인트에 도달할때마다 다음 체크포인트가 활성화됩니다. 행인과 충돌시 마지막 체크포인트로 돌아가는 기능을
    만들었습니다.
    
    ## Screenshot
    
    <!-- 개발해서 적용된 부분 스크린샷이나 플레이 영상을 첨부해주세요. 바뀐거 없으면 스킵 -->
    
    ## Review Points
    
    <!-- 리뷰어가 중점적으로 봐줬으면 하는 부분을 알려주세요. -->
    
    ## Notion Task Link
    
    <!-- 무슨 태스크를 진행한 건지, 노션 태스크 링크를 첨부해주세요. 없으면 스킵 -->

---
+++ SchoolRush/Assets/Scripts/CheckpointIdentifier.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class CheckpointIdentifier : MonoBehaviour
+{
+    [Tooltip("1~5는 체크포인트, 6은 도착 지점")]
+    public int ID;
+}
---
+++ SchoolRush/Assets/Scripts/Kart/CheckpointManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System;
+
+public class CheckpointManager : MonoBehaviour
+{
+    [Header("ID 순서대로 1~6 체크포인트 오브젝트를 넣어주세요")]
+
+    public GameObject checkpointPosition;
+    public GameObject[] checkpoints;  // 0→ID=1, … , 5→ID=6(도착지점)
+
+    private int _current = 0;          // 지금까지 통과한 마지막 cp ID (0=start)
+    public int CurrentCheckpoint => _current;
+
+
+    void Start()
+    {
+        for (int i = 0; i < checkpoints.Length; i++)
+            checkpoints[i].SetActive(false);
+
+        if (checkpoints.Length > 0)
+            checkpoints[0].SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Checkpoint")) return;
+
+        int cpId = other.GetComponent<CheckpointIdentifier>().ID;
+        if (cpId == _current + 1)
+        {
+            checkpoints[_current].SetActive(false);
+            _current++;
+            Debug.Log($"체크포인트 {_current} 통과!");
+            checkpointPosition = checkpoints[_current - 1];
+
+            if (_current < checkpoints.Length)
+            {
+                checkpoints[_current].SetActive(true);
+            }
+
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Passenger"))
+        {
+            GoToCheckPoint();
+        }
+    }
+
+    private void GoToCheckPoint()
+    {
+        if (checkpointPosition == null) return;
+        transform.position = checkpointPosition.transform.position;
+    }
+}
