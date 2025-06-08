fcb3208b5762f192b6be8a69e10a15b81d226f7b
mn39 <48751102+mn39@users.noreply.github.com>
2025-06-08 10:50:40 +0900

    traffic (#85)
    ## Description
    
    충돌을 우려해 그냥 폴더를 따로 파서 만들었습니다.
    project/PrequeTraffic/Scenes/MainDemo에서 확인하실 수 있습니다.
    
    
    <!-- 전체적인 내용입니다. -->
    
    waypoint를 지정하는 방식을 베이스로 만들었습니다.
    
    ## Screenshot
    
    <!-- 개발해서 적용된 부분 스크린샷이나 플레이 영상을 첨부해주세요. 바뀐거 없으면 스킵 -->
    
    
    https://github.com/user-attachments/assets/7fd4665e-f181-41e1-a413-8326d2c43e63
    
    
    
    ## Review Points
    
    <!-- 리뷰어가 중점적으로 봐줬으면 하는 부분을 알려주세요. -->
    
    ## Notion Task Link
    
    <!-- 무슨 태스크를 진행한 건지, 노션 태스크 링크를 첨부해주세요. 없으면 스킵 -->
    
    ---------
    
    Co-authored-by: woohm402 <woohm404@gmail.com>

---
+++ SchoolRush/Assets/PequeTraffic/Scripts/NavigatorSpawner.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Peque.Traffic
+{ 
+    public class NavigatorSpawner : MonoBehaviour
+    {
+        public enum Direction
+        {
+            Both = 2,
+            Normal = 0,
+            Inverse = 1,
+        }
+        public GameObject[] prefabs;
+        public int numberToSpawn = 5;
+        [Tooltip("Number of attempts that spawner will try to instantiate the requested amount of prefabs.")]
+        public int maxAttempts = 10;
+        public Direction allowedDirection = Direction.Both;
+        [Tooltip("Waypoint gameobjects will be removed at runtime to improve fps")]
+        public bool optimizeOnRuntime = true;
+        [Tooltip("Waypoint gameobjects removal at runtime will also be done in Editor")]
+        public bool optimizeOnEditorToo = true;
+
+        private List<WaypointData> waypoints;
+
+        void Start() {
+            getChildWaypoints();
+
+            if (numberToSpawn > 0 && waypoints.Count > 0) {
+                StartCoroutine(spawn());
+            }
+        }
+
+        private void getChildWaypoints() {
+            waypoints = new List<WaypointData>();
+
+            foreach (Waypoint waypoint in transform.GetComponentsInChildren<Waypoint>()) {
+                waypoints.Add(waypoint.data);
+            }
+        }
+
+        IEnumerator spawn () {
+            int count = 0;
+            int attempts = 0; 
+
+            while (count < numberToSpawn) {
+                WaypointData randomWaypoint = getRandomWaypoint();
+
+                // seems like there are no available slots
+                if (randomWaypoint == null) {
+                    //Debug.Log("No available slots found for " + transform.name + " waiting a second");
+                    attempts++;
+
+                    if (attempts == maxAttempts) {
+                        //Debug.Log("No available slots found for " + transform.name + ", stopping spawner.");
+                        break;
+                    }
+
+                    yield return new WaitForSeconds(1);
+                    continue;
+                }
+				// make sure this waypoint isn't reused for spawning more entities
+                randomWaypoint.occupied = true;
+
+                GameObject obj = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
+
+                Vector3 spawnPosition = randomWaypoint.centerPosition;
+                spawnPosition.y += 0.5f;
+
+                obj.transform.position = spawnPosition;
+
+				// Point spawned entities looking at their next waypoint
+                Vector3 lookPos = spawnPosition;
+                if (randomWaypoint.nextWaypoint != null) {
+                    lookPos = randomWaypoint.nextWaypoint.centerPosition;
+                } else if(randomWaypoint.previousWaypoint != null) {
+                    lookPos = randomWaypoint.previousWaypoint.centerPosition;
+                }
+
+                lookPos.y = obj.transform.position.y;
+                obj.transform.LookAt(lookPos);
+
+
+                int direction;
+
+                if (allowedDirection == Direction.Both) {
+                    direction = Mathf.RoundToInt(Random.Range(0f, 1f));
+                } else {
+                    direction = (int)allowedDirection;
+                }
+
+                obj.GetComponent<WaypointNavigator>().init(direction, randomWaypoint);
+
+                yield return new WaitForEndOfFrame();
+                count++;
+            }
+
+            if (optimizeOnRuntime && (!Application.isEditor || (Application.isEditor && optimizeOnEditorToo))) {
+                removeWaypointsGameobjects();
+            }
+        }
+
+        void removeWaypointsGameobjects () {
+            foreach (Transform child in transform) {
+                Destroy(child.gameObject);
+            }
+        }
+
+        WaypointData getRandomWaypoint (int attempt = 0) {
+            WaypointData waypoint = waypoints[Random.Range(0, waypoints.Count - 1)];
+
+            // to avoid overlapping on spawn, check if current or nearest waypoints are already occupied
+            if (waypoint.occupied || (waypoint.nextWaypoint != null && waypoint.nextWaypoint.occupied) || (waypoint.previousWaypoint != null && waypoint.previousWaypoint.occupied)) {
+                attempt++;
+
+                if (attempt == maxAttempts) {
+                    return null;
+                }
+
+                return getRandomWaypoint(attempt);
+            }
+
+            return waypoint;
+        }
+    }
+}
