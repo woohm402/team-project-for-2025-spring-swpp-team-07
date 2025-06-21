a42b4becb9716ae12ca0a2a39ded280b4d5a77ee
SeungYeon <95578121+cloNoey@users.noreply.github.com>
2025-06-21 18:49:20 +0900

    Fix/UI/main scene (#160)
    ## Description
    
    <!-- 전체적인 내용입니다. -->
    메인씬 UI 개선
    - 버튼 개선
    - 닉네임 입력 안했을 때 시작 불가능
    
    ## Screenshot
    
    <!-- 개발해서 적용된 부분 스크린샷이나 플레이 영상을 첨부해주세요. 바뀐거 없으면 스킵 -->
    <img width="846" alt="스크린샷 2025-06-21 오후 6 38 14"
    src="https://github.com/user-attachments/assets/b26880b1-46cd-403b-801a-3f0b799b066c"
    />
    
    ## Review Points
    
    <!-- 리뷰어가 중점적으로 봐줬으면 하는 부분을 알려주세요. -->
    
    ## Notion Task Link
    
    <!-- 무슨 태스크를 진행한 건지, 노션 태스크 링크를 첨부해주세요. 없으면 스킵 -->

--- SchoolRush/Assets/Scripts/UI/SceneManagerCS.cs
+++ SchoolRush/Assets/Scripts/UI/SceneManagerCS.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneManagerCS : MonoBehaviour {
+    public GameObject helpImage;
+    public TMP_InputField nicknameInput;
+
     public void loadScene(string sceneName) {
+        string nickname = nicknameInput.text.Trim();
+        if (helpImage.activeInHierarchy) return;
+        if (string.IsNullOrEmpty(nickname)) return;
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
     }
 }
